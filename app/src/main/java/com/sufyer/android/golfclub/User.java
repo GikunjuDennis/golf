f8e5efddf21ee45c64566118b407b02a703e6349
david dut <dutdavid@hotmail.com>
2021-09-27 02:13:26 +0300

    first commit

---
+++ app/src/main/java/com/sufyer/android/golfclub/User.java
@@ -0,0 +1,17 @@
+package com.sufyer.android.golfclub;
+
+public class User {
+
+    private final String username;
+
+
+    public User(String username) {
+        this.username = username;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
